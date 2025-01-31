d1bdfa10029fee2d1706f0a9459e1898986feb17
Alex Carl <alexjcarl97@gmail.com>
2025-01-30 19:27:12 -0500

    favorite recipes and recipes models add to be

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,5 @@ from .db import db
 from .user import User
 from .db import environment, SCHEMA
 from .grocery_list import GroceryList
+from .recipe import Recipe
+from .favorite_recipe import FavoriteRecipe
---
+++ app/models/recipe.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+
+class Recipe(db.Model):
+    __tablename__ = 'recipes'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.ARRAY(db.String), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+    updated_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'instructions': self.instructions,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
