06c9654937acdf24de46b69227c45f595fb3c225
Alex Carl <alexjcarl97@gmail.com>
2025-02-04 22:53:13 -0500

    openai is fully functional

--- app/api/thechef.py
+++ app/api/thechef.py
@@ -9,7 +9,7 @@ import os
 load_dotenv()
 
 
-random_food = ['milk', 'rice', 'chicken' ]
+test_case = ['onion', 'rice', 'chicken', 'mushroom soup' ]
 
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 
@@ -23,11 +23,11 @@ completion = client.beta.chat.completions.parse(
     model='gpt-4o-mini-2024-07-18',
     messages=[
         {'role': 'system', 'content': 'You are an expert at structured data extraction. You will be given a list of ingrediants and give a recipe back in the given structure.'},
-        {'role': 'user', 'content': "".join(random_food)}
+        {'role': 'user', 'content': "".join(test_case)}
     ],
     response_format=Chef,
 )
 
 recipe = completion.choices[0].message.parsed
 
-print(recipe)
+
