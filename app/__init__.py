a2a4e75dfac6d2d993ffbae071d484e1ebf72d58
Alex Carl <alexjcarl97@gmail.com>
2025-02-16 12:58:17 -0500

    Feat: added routes so users can add recipes to their favorites as well as remove them

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
-from .api.thechef import recipe_routes
+from .api.recipes import recipe_routes
 from .api.items import item_routes
 from .seeds import seed_commands
 from .config import Config
