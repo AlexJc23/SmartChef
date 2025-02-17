0d78e7f19a348543732d7836a2c37c105a396ad8
Alex Carl <alexjcarl97@gmail.com>
2025-02-16 21:10:06 -0500

    Feat: Grab current users grocery list info

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .api.user_routes import user_routes
 from .api.auth_routes import auth_routes
 from .api.recipes import recipe_routes
 from .api.items import item_routes
+from .api.grocery_lists import lists_routes
 from .seeds import seed_commands
 from .config import Config
 
@@ -32,6 +33,7 @@ app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
 app.register_blueprint(recipe_routes, url_prefix= '/api/recipe')
 app.register_blueprint(item_routes, url_prefix='/api/items')
+app.register_blueprint(lists_routes, url_prefix='/api/lists')
 
 db.init_app(app)
 Migrate(app, db)
